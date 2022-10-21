1ef652f7480dc09cbe3029faa23a7dffad5c751e
quoctrungvu1610 <80931191+quoctrungvu1610@users.noreply.github.com>
2022-10-21 21:38:46 +0700

    Display EXP
    Save and Display EXP

--- Assets/Scripts/Attributes/Experience.cs
+++ Assets/Scripts/Attributes/Experience.cs
@@ -1,15 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RPG.Saving;
 
 namespace RPG.Attributes
 {
-    public class Experience : MonoBehaviour
+    public class Experience : MonoBehaviour,ISaveable
     {
         [SerializeField] float experiencePoints = 0;
+
+        public object CaptureState()
+        {
+            return experiencePoints;
+        }
+
         public void GainExperience(float experience)
         {
             experiencePoints += experience;
+        }
+
+        public void RestoreState(object state)
+        {
+           experiencePoints = (float)state;
+        }
+        //137 EXP
+        public float GetPoints()
+        {
+            return experiencePoints;
         }
     }
 }
